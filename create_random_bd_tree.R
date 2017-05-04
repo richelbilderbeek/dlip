477da2fceeff3a4c0c28db1b66e5dfd991c1be9c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-04 12:54:39 +0200

    Show how a phylogeny is represented in memory

--- create_random_bd_tree.R
+++ create_random_bd_tree.R
@@ -4,6 +4,7 @@ library(geiger)
 # Create a full tree
 # from a Birth-Death model
 # that stops after a certain number of taxa
+set.seed(42)
 birth_rate <- 0.2
 death_rate <- 0.0
 n_taxa <- 10
@@ -17,6 +18,12 @@ tree_reconstructed <- geiger::drop.extinct(tree_full) # Drop extinct
 if (1 == 2) {
   ape::plot.phylo(tree_reconstructed)
 }
+
+labels(tree_reconstructed)
+print(tree_reconstructed$edge)
+print(tree_reconstructed$Nnode)
+print(tree_reconstructed$tip.label)
+print(tree_reconstructed$edge.length)
 
 write.tree(tree_reconstructed,file = "~/newick.txt")
 
